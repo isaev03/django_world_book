410e805a9ba5c86a5aa8197bc22738dc5e4c2eda
isaev03 <gamzat-isaev@inbox.ru>
2022-11-19 17:22:55 +0300

    fix bugs

--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['my-world-book-iga.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'my-world-book-iga.herokuapp.com']
 
 
 # Application definition
